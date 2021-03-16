f52f4e758f13d88064c7ef29e03fe6aa4812c875
nickgs <nick@segosolutions.com>
2021-03-16 22:21:51 +0000

    Up to lesson 3 of ethereum front end series

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ let accounts;
 
 window.onload = function() {
     console.log("DApp is loaded");
+
+    if(window.ethereum) {
+        // we can access web3!
+        this.ethereum.on('accountsChanged', handleAccountsChanged);
+
+        window.ethereum.request({ method: 'eth_accounts'})
+            .then(handleAccountsChanged)
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    else {
+        console.log("Please install a digital wallet like Metamask");
+    }
+}
+
+const handleAccountsChanged = (a) => {
+    console.log("Accounts changed");
+    accounts = a;
 }
 
 const enableEth = async () => {
